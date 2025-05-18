8343a838eddfceba88b0c55d91dbeb6961ebb303
Atulya Minz <atulminz.jsr@gmail.com>
2025-05-19 04:20:02 +0530

    Added cookie storage

--- web/src/lib/lens/client.tsx
+++ web/src/lib/lens/client.tsx
@@ -1,10 +1,44 @@
 import { mainnet, PublicClient, testnet } from "@lens-protocol/client";
+import { clientCookieStorage, cookieStorage } from "./storage";
+
+const isServer = typeof window === "undefined";
 
 export const client = PublicClient.create({
   environment: mainnet,
   apiKey: process.env.LENS_API,
+  storage: isServer ? cookieStorage : clientCookieStorage,
 });
 
 export const getPublicClient = () => {
   return client;
+};
+
+export const getBuilderClient = async (
+  address: string,
+  signMessage: (message: string) => Promise<string>
+) => {
+  if (!address) {
+    return null;
+  }
+
+  const authenticated = await client.login({
+    builder: {
+      address: address,
+    },
+    signMessage,
+  });
+
+  if (authenticated.isErr()) {
+    throw authenticated.error;
+  }
+
+  return authenticated.value;
+};
+
+export const getLensClient = async () => {
+  const resumed = await client.resumeSession();
+  if (resumed.isErr()) {
+    return client;
+  }
+  return resumed.value;
 };
