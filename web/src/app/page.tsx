aa6f513b908ce65e80d39d57441b811861ff6480
Atulya Minz <atulminz.jsr@gmail.com>
2025-05-20 04:56:44 +0530

    Updated Landing Page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,29 +1,9 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { ConnectKitButton } from "connectkit";
-import Image from "next/image";
+import HeroSection from "@/components/pages/hero";
 
 export default function Home() {
   return (
-    <div>
-      <ConnectKitButton.Custom>
-        {({
-          isConnected,
-          isConnecting,
-          show,
-          address,
-          ensName,
-          hide,
-          truncatedAddress,
-        }) => {
-          return (
-            <Button onClick={show}>
-              {isConnected ? truncatedAddress : "Connect"}
-            </Button>
-          );
-        }}
-      </ConnectKitButton.Custom>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 }
