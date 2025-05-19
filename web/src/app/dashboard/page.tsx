00641b09ba039296e3590b74f6cdf9ad283dedb9
Atulya Minz <atulminz.jsr@gmail.com>
2025-05-20 05:09:57 +0530

    Updated

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,14 +1,3 @@
-import { getLensClient } from "@/lib/lens/client";
-
-import { getAuthenticatedAccount } from "../page";
-
-export default async function Dashboard() {
-  const account = await getAuthenticatedAccount();
-  if (account) {
-    return (
-      <div>
-        <p>{JSON.stringify(account?.metadata?.name)}</p>
-      </div>
-    );
-  }
+export default function Dashboard() {
+  return <>Dashboard</>;
 }
